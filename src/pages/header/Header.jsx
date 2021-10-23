6bef4770b44f6d8e30f71dbad1ebb02db3fcc8a3
MarceloSilva05 <jmarcelosilva.2021@gmail.com>
2021-10-23 19:09:10 -0300

    Cria os componentes header e footer

---
+++ src/pages/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css"
+export default function Header(){
+    return(
+        <header>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Personagens</a></li>
+            </ul>
+        </nav>
+        </header>
+    )
+};
